cd659245287e43e73c93a5ebcfceecb66a523300
syumai <syumai@gmail.com>
2019-08-29 16:18:57 +0900

    Updated patch

--- deno-patcher.ts
+++ deno-patcher.ts
@@ -4,9 +4,7 @@ const dec = new TextDecoder('utf-8');
   let tsBody = dec.decode(await Deno.readFile('lib/typescript.js'));
   tsBody = tsBody.split('\n').filter(l => !l.match(/require\(/)).join('\n');
   tsBody += `
-export {
-  ts,
-};
+export default ts;
 `
   await Deno.writeFile('lib/typescript-patched.js', enc.encode(tsBody));
 })();
